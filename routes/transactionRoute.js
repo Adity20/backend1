91877078cb0bda07a2d432822cb8ce1277f42b9e
aditya <sharma.aditya9164@gmail.com>
2024-11-15 22:23:24 +0530

    chore: Remove price and transaction controllers, models, and routes

--- routes/transactionRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { fetchTransactions, getExpenses } from '../controllers/transactionController.js';
-
-const router = Router();
-
-router.post('/transactions', fetchTransactions);
-
-export default router;
